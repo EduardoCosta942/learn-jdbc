cbf6f953c5dfcb5cfa355b31f24204765e13b543
Eduardo Costa <eduardocosta942z@gmail.com>
2025-08-28 07:00:57 -0300

    feat: Connection to database

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Connection connection = null;
+        if (!connection.connect().equals(null)){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
