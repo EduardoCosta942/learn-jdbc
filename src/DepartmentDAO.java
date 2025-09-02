fa08ca476b8738ac94a1bdeb779ffb7031d70799
Eduardo Costa <eduardocosta942z@gmail.com>
2025-09-02 16:17:30 -0300

    feat: DepartmentDAO logic and methods

--- src/DepartmentDAO.java
+++ src/DepartmentDAO.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class DepartmentDAO {
     public boolean insert(){
@@ -11,14 +10,66 @@ public class DepartmentDAO {
             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO DEPT" +
                     "(DEPTNO,DNAME,LOC) VALUES (?,?,?)");
 
-            pstmt.setInt(1, 90);
+            pstmt.setInt(1, 91);
             pstmt.setString(2, "Marketing");
-            pstmt.setString(3, "Sao Paulo");
+            pstmt.setString(3, "RJ");
             pstmt.execute();
+            conexao.disconnect(conn);
             return true;
+        } catch (SQLException sqle){
+            sqle.printStackTrace();
+            conexao.disconnect(conn);
+            return false;
+        }
+    }
+    public boolean update(){
+        Conexao conexao = new Conexao();
+        Connection conn = conexao.connect();
+
+        try{
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "UPDATE DEPT SET NAME ? WHERE DEPTNO = ?"
+            );
+            preparedStatement.setString(1, "Producao");
+            preparedStatement.setInt(2, 90);
+            preparedStatement.execute();
+            conexao.disconnect(conn);
         } catch (SQLException sqle){
             sqle.printStackTrace();
             return false;
         }
+        return true;
+    }
+    public boolean remove(){
+        Conexao conexao = new Conexao();
+        Connection conn = conexao.connect();
+
+        try{
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "DELETE FROM DEPT WHERE DEPTNO = ?"
+            );
+            preparedStatement.setInt(1, 91);
+            preparedStatement.execute();
+            conexao.disconnect(conn);
+        } catch (SQLException sqle){
+            sqle.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public ResultSet search(){
+        Conexao conexao = new Conexao();
+        Connection conn = conexao.connect();
+        ResultSet rs = null;
+
+        try{
+            Statement stmnt = conn.createStatement();
+            rs = stmnt.executeQuery("SELECT * FROM DEPT ORDER BY DEPTNO");
+            conexao.disconnect(conn);
+        } catch (SQLException sqle){
+            sqle.printStackTrace();
+        }
+        return rs;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Conexao connection = new Conexao();
-        Connection conn = connection.connect();
+        Conexao conexao = new Conexao();
+        Connection conn = conexao.connect();
         System.out.println(conn != null);
-        connection.disconnect(conn);
+        conexao.disconnect(conn);
 
         DepartmentDAO deptDao= new DepartmentDAO();
-        System.out.println(deptDao.insert());
-        connection.disconnect(conn);
+/*        System.out.println(deptDao.insert());
+        conexao.disconnect(conn);*/
+
+
+        ResultSet rs = deptDao.search();
+        try{
+            while (rs.next()){
+                String line = "depto{"
+                        + "DEPTNO=" + rs.getInt("DEPTNO")
+                        + ", DNAME=" + rs.getString("DNAME")
+                        + ", LOC=" + rs.getString("LOC")
+                        + "};";
+                System.out.println(line);
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
     }
 }
