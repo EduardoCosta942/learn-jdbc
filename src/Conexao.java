dae0e6e1dbfbb9c6feaf3cc617a2392a2ba0d199
Eduardo Costa <eduardocosta942z@gmail.com>
2025-09-02 16:15:10 -0300

    maint: added environment variables and fixed database connection logc

--- src/Conexao.java
+++ src/Conexao.java
@@ -1,15 +1,18 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
     public Connection connect(){
+        Dotenv dotenv = Dotenv.load();
         Connection connection = null;
         try{
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/sales_db",
-                    "admin", "admin"
+                    dotenv.get("database_url"),
+                    dotenv.get("database_user"), dotenv.get("database_password")
             );
         } catch (ClassNotFoundException cnfe){
             cnfe.printStackTrace();
