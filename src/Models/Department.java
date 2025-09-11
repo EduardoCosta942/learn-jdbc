31110fc90b1e5eeccc12ba8580a2be946a663aa0
Eduardo Costa <eduardocosta942z@gmail.com>
2025-09-11 16:11:58 -0300

    feat: class Department for models

--- src/Models/Department.java
+++ src/Models/Department.java
@@ -1,4 +1,47 @@
 package Models;
 
 public class Department {
+    // Declare attributes
+    private final int deptno;
+    private String dname;
+    private String dloc;
+
+    // Method builder
+    public Department(int deptno, String dname, String dloc){
+        this.deptno = deptno;
+        this.dname = dname;
+        this.dloc = dloc;
+    }
+
+    // Getters Methods
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public String getDloc() {
+        return dloc;
+    }
+
+    // Setters Methods
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public void setDloc(String dloc) {
+        this.dloc = dloc;
+    }
+
+    // Method toString()
+    @Override
+    public String toString() {
+        return "Department{" +
+                "deptno=" + deptno +
+                ", dname='" + dname + '\'' +
+                ", dloc='" + dloc + '\'' +
+                '}';
+    }
 }
